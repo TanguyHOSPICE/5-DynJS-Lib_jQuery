46fbdfe5412415853367d9d9d38a7667450c85f2
Ht <htcvhs971@gmail.com>
2023-02-22 10:47:23 +0100

    Apply new  h1  color title Display new h2 w/ style

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //*?Test présence jQuery
 if (jQuery) {
 	console.log('jQuery est inclus à la page');
+	$(() => {
+		$('h1').css('color', 'green');
+		$('h2').first().css('font-style', 'italic').text('Mes meilleurs articles');
+	});
 } else {
 	console.log("jQuery n'est  pas inclus à la page");
 }
